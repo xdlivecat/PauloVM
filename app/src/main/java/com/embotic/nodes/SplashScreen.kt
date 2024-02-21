6544738a9f4dbaadce6b977766796a1b60c55e28
xdlivecat <xdliverblx@hotmail.com>
2024-02-21 15:14:07 +0100

    Fixed SplashScreen.kt and AndroidManifest.xml, also fixed build issues.

--- app/src/main/java/com/embotic/nodes/SplashScreen.kt
+++ app/src/main/java/com/embotic/nodes/SplashScreen.kt
@@ -1,39 +1,46 @@
-package com.embotic.nodes
-
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.embotic.nodes.MainActivity
+import com.embotic.nodes.R
 
 class SplashScreen : AppCompatActivity() {
 
     private lateinit var progressBar: ProgressBar
-    private lateinit var textViewLoading : TextView
+    private lateinit var textViewLoading: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // Initialize UI elements
         progressBar = findViewById(R.id.progressBar)
         textViewLoading = findViewById(R.id.textViewLoading)
 
+        // Start the loading animation
         startLoadingAnimation()
 
+        // Perform your initial tasks here, such as data initialization or network calls
+        // After completing the tasks, call the 'goToMainActivity()' method
         Handler().postDelayed({
             goToMainActivity()
-        }, 3000)
+        }, 3000) // 3000ms = 3 seconds
     }
+
     private fun startLoadingAnimation() {
         progressBar.visibility = View.VISIBLE
         textViewLoading.visibility = View.VISIBLE
     }
+
     private fun stopLoadingAnimation() {
         progressBar.visibility = View.GONE
         textViewLoading.visibility = View.GONE
     }
+
     private fun goToMainActivity() {
         stopLoadingAnimation()
         val intent = Intent(this, MainActivity::class.java)
